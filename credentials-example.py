79a9d3124d3402046c058989c5f1c72f11c217ab
Greenhouse Analytics <49034498+greenhouse-analytics@users.noreply.github.com>
2019-12-04 13:57:24 +0100

    Update credentials-example.py

--- credentials-example.py
+++ credentials-example.py
@@ -2,9 +2,12 @@
 client_id = ''
 client_secret = ''
 scopes = ['https://www.googleapis.com/auth/spreadsheets']
-redirect_uri = ''
+redirect_uri = 'urn:ietf:wg:oauth:2.0:oob'
 user_agent = 'Analytics Python Developement Client Library'
 
-# get tese by running authorization.py
+# get these by running authorization.py
 access_token = ''
 refresh_token = ''
+
+## Scopes:
+## https://developers.google.com/identity/protocols/googlescopes
